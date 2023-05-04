a8070754e5f8943d6da6aaf222799ec30533767a
Bruno 'Borta' Bortagaray <khalisto@gmail.com>
2023-05-05 01:32:59 +0200

    feat: Finish the unit 2 with the invert exercise

--- src/components/Canvas/index.jsx
+++ src/components/Canvas/index.jsx
@@ -2,41 +2,41 @@ import { useLayoutEffect, useRef, useState } from "react";
 
 function Canvas({children}) {
     const canvasRef = useRef(null);
+    const width = 1080;
+    const height = 1080;
 
     useLayoutEffect(() => {
         const context = canvasRef.current.getContext('2d');
 
-        // context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        context.fillStyle = 'black';
+        context.strokeStyle = 'white';
+        context.fillRect(0, 0, width, height);
 
-        context.lineWidth = 4;
-        context.beginPath();
-        context.rect(100, 100, 400, 400);
-        // context.stroke();
-
-        context.beginPath();
-        context.arc(300, 300, 100, 0, Math.PI * 2);
-        // context.stroke();
-
-        const width = 60;
-        const height = 60;
-        const x = 100;
-        const y = 100;
-        const gap = 80;
+        const w = width * 0.10;
+        const h = height * 0.10;
+        const gap = width * 0.03;
+        const initialX = width * 0.17;
+        const initialY = height * 0.17;
+        const off = width * 0.02;
+        let x,y;
         for(let i = 0; i < 5; i++) {
             for(let j = 0; j < 5; j++) {
 
+                x = initialX + (w + gap) * i;
+                y = initialY + (h + gap) * j;
                 context.beginPath();
-                context.rect(x + gap * i, y + gap * j, width, height);
+                context.rect(x, y, w, h);
                 if(Math.random() > 0.5) {
-                    context.rect((x + gap * i) + 8, (y + gap * j) + 8, width - 16, height - 16);
+                    context.rect(x + off/2, y + off/2, w - off, h - off);
                 }
+                // context.closePath();
                 context.stroke();
             }
 
         }
 
     }, []);
-    return <canvas width="1080" height="1080" ref={canvasRef}>{children}</canvas>;
+    return <canvas width={width} height={height} ref={canvasRef}>{children}</canvas>;
 }
 
 export default Canvas;
